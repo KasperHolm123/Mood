c4e14052fc99a40029df0a8005e62e15b6cfc865
Kasper Holm <kasper.marhauer@gmail.com>
2023-01-08 14:14:44 +0100

    Directory exists check

--- Mood/MauiProgram.cs
+++ Mood/MauiProgram.cs
@@ -39,6 +39,10 @@ public static class MauiProgram
 
         #region Database construction
         // Making sure the target device has database
+        if (!Directory.Exists("storage/emulated/0/Android/data/com.companyname.mood/files"))
+        {
+            Directory.CreateDirectory("storage/emulated/0/Android/data/com.companyname.mood/files");
+        }
         if (!File.Exists(Path.Combine("storage/emulated/0/Android/data/com.companyname.mood/files", "db_sqlite.db3")))
 		{
 			DatabaseConstructor.Up();
